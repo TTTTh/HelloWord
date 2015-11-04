ae0f2ae918771fcac2b799616c5b82487e76c158
滕海滨 <tenghaibin@tenghaibindemacbook-pro.local>
2015-11-04 10:32:38 +0800

    Initial Commit

---
+++ HelloWord/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWord
+//
+//  Created by 滕海滨 on 15/11/4.
+//  Copyright © 2015年 滕海滨. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
